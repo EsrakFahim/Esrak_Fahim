d6a376ea9f634301ccc4abbadaeeb0126409b16d
EsrakFahim <efahim0656@gmail.com>
2024-09-17 01:51:17 +0600

    Enhance HeroSection with new layout and gradient effects

--- app/Components/HeroSection/HeroSection.js
+++ app/Components/HeroSection/HeroSection.js
@@ -7,15 +7,20 @@ import { ScrollTrigger } from "gsap/all";
 import TextSlider from "@/app/AnimComponents/TextSlider/TextSlider";
 
 const HeroSection = () => {
-      
       return (
             <div className="h-screen bg-primary relative  flex overflow-hidden">
-                  <div
-                        className="absolute flex flex-col gap-1 pointer-events-none "
-                  >
-                        <TextSlider text="Innovative Solutions -" direction={-1} />
-                        <TextSlider text="Cutting-Edge Technology -" direction={1} />
-                        <TextSlider text="Esrak Fahim -" direction={-1} />
+                  <div className="relative left-1/2 -translate-x-1/2  rounded-full w-[50vw] h-full overflow-hidden hero-effect">
+                        <div className="absolute flex flex-col gap-1 pointer-events-none ">
+                              <TextSlider
+                                    text="Innovative Solutions -"
+                                    direction={-1}
+                              />
+                              <TextSlider
+                                    text="Cutting-Edge Technology -"
+                                    direction={1}
+                              />
+                              <TextSlider text="Esrak Fahim -" direction={-1} />
+                        </div>
                   </div>
             </div>
       );
